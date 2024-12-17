0b688b8118326b889e738b419c56e8c69968ee1a
Lyncoln Campello <lyncolncamp@gmail.com>
2024-12-17 16:51:23 -0300

    Criando crud da entidade corrida

--- src/main/java/com/riderx/riderx/repository/CorridaRepository.java
+++ src/main/java/com/riderx/riderx/repository/CorridaRepository.java
@@ -1,5 +1,15 @@
 package com.riderx.riderx.repository;
 
-public interface CorridaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.riderx.riderx.model.Corrida;
+
+@Repository
+public interface CorridaRepository extends JpaRepository<Corrida, Long> {
+
+	public List <Corrida> findAllByDestinoContainingIgnoreCase(@Param("destino") String destino);
 }
